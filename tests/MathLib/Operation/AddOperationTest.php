0e16d07ecfbb94a4ea1064ebb9cb904665d43a98
Clement Keirua <clement@keiruaprod.fr>
2014-07-06 12:25:39 +0200

    Added sample test

---
+++ tests/MathLib/Operation/AddOperationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class AddOperationTest extends PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        // Initialisation
+        $addOperation = new MathLib\Operation\AddOperation;
+
+        // Realisation
+        $result = $addOperation->execute(3, 4);
+
+        // Verification
+        $this->assertEquals(7, $result);
+    }
+}
