30dc1ab6cea64bb190c10a5f8d8c733d3609ec54
Clement Keirua <clement@keiruaprod.fr>
2014-07-06 10:30:18 +0200

    one file with abstract

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
-echo "test".PHP_EOL;
+abstract class Operation {
+    public function execute($a, $b){
+        echo "code from abstract class".PHP_EOL;
+    }
+};
+
+class MulOperation extends Operation{
+    public function execute($a, $b){
+        parent::execute($a, $b);
+        return $a * $b;
+    }
+};
+
+class AddOperation extends Operation{
+    public function execute($a, $b){
+        parent::execute($a, $b);
+        return $a + $b;
+    }
+};
+
+$addOperation = new AddOperation ();
+echo $addOperation->execute (2,3).PHP_EOL;
+
+$mulOperation = new MulOperation ();
+echo $mulOperation->execute (2,3).PHP_EOL;
