db54c29607d8d8ddd4dd28e21da33c6769536220
Clement Keirua <clement@keiruaprod.fr>
2014-07-06 10:44:24 +0200

    Added exception management

--- index.php
+++ index.php
@@ -25,16 +25,24 @@ class OperationFactory{
         elseif ($operationName === 'mul'){
             $operation = new MulOperation();
         }
+
+        if ($operation == null){
+            throw new LogicException ("Le type demandé n'existe pas".PHP_EOL);
+        }
         return $operation;
     }
 };
 
 $factory = new OperationFactory();
-$addOperation = $factory->getOperation ('add');
-echo $addOperation->execute (2,3).PHP_EOL;
+try {
+    $addOperation = $factory->getOperation ('add');
+    echo $addOperation->execute (2,3).PHP_EOL;
 
-$mulOperation = $factory->getOperation ('mul');
-echo $mulOperation->execute (2,3).PHP_EOL;
+    $mulOperation = $factory->getOperation ('mul');
+    echo $mulOperation->execute (2,3).PHP_EOL;
 
-$mulOperation = $factory->getOperation ('plop');
-echo $mulOperation->execute (2,3).PHP_EOL;
+    $mulOperation = $factory->getOperation ('plop');
+    echo $mulOperation->execute (2,3).PHP_EOL;
+}catch (Exception $e) {
+    echo $e->getMessage();
+}
